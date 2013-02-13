19ee78b66744ec750b22d6667518249d151a7ae0
Cal Paterson <cal@calpaterson.com>
2013-02-13 15:33:30 +0000

    Implement a basic tracker

---
+++ src/server.py
@@ -0,0 +1,21 @@
+import cProfile
+
+from gevent import monkey; monkey.patch_all()
+import bottle
+
+from thehighseas import (
+    tracker
+)
+
+def main():
+    try:
+        app = bottle.Bottle()
+        app.mount("/tracker", tracker.app)
+        bottle.run(port=11235, server="gevent", app=app)
+    except KeyboardInterrupt:
+        print("Quitting!")
+
+if __name__ == "__main__":
+    main()
+    # cProfile.run('main()', 'results')
+
---
+++ src/thehighseas/tracker.py
@@ -0,0 +1,98 @@
+from base64 import b64encode, b64decode
+
+from bencode import bencode, bdecode
+from bottle import Bottle, request, response, abort
+from redis import StrictRedis
+import simplejson
+import time
+
+app = Bottle()
+
+_redis_connection_ = StrictRedis()
+
+_tracker_id_ = "thehighseas"
+
+_interval_ = 5
+
+def build_listing(announcement):
+    this_peer = Peer(announcement)
+    info_hash = announcement["info_hash"].encode("hex") + ".swarm"
+    _redis_connection_.hset(info_hash, this_peer.peer_id,
+                            this_peer.to_json())
+    
+    listing = {
+        "tracker id": _tracker_id_,
+        "interval": _interval_,
+        "complete": 0,
+        "incomplete": 0,
+        "peers": []
+        }
+
+    values = _redis_connection_.hvals(info_hash)
+    peers = [Peer.from_json(e) for e in values]
+    for peer in peers:
+        if peer.is_complete():
+            listing["complete"] += 1
+        else:
+            listing["incomplete"] += 1
+        listing["peers"].append(peer.to_dict())
+    return bencode(listing)
+
+class Peer(object):
+    def __init__(self, announcement=None):
+        if announcement is not None:
+            self.peer_id = announcement["peer_id"]
+            self.port = announcement["port"]
+            if announcement["left"] == "0":
+                self.status = "complete"
+            else:
+                self.status = "incomplete"
+            self.last_seen = int(time.time())
+            try:
+                self.ip = announcement["ip"]
+            except KeyError:
+                self.ip = request.remote_addr
+            self.uploaded = int(announcement["uploaded"])
+            self.downloaded = int(announcement["downloaded"])
+            self.left = int(announcement["left"])
+
+    def __eq__(self, other):
+        return self.peer_id == other.peer_id
+
+    def is_complete(self):
+        return self.status == "complete"
+
+    def to_json(self):
+        return simplejson.dumps(
+            {"peer_id": b64encode(self.peer_id),
+             "ip": self.ip,
+             "port": self.port,
+             "status": self.status,
+             "last_seen": self.last_seen,
+             "uploaded": self.uploaded,
+             "downloaded": self.downloaded,
+             "left": self.left})
+
+    def to_dict(self):
+        return {"peer_id": self.peer_id,
+                "ip": self.ip,
+                "port": self.port}
+
+    @classmethod
+    def from_json(cls, json_string):
+        dict_ = simplejson.loads(json_string)
+        peer = Peer()
+        peer.peer_id = b64decode(dict_["peer_id"])
+        peer.ip = dict_["ip"]
+        peer.port = dict_["port"]
+        peer.status = dict_["status"]
+        peer.last_seen = dict_["last_seen"]
+        peer.uploaded = dict_["uploaded"]
+        peer.downloaded = dict_["downloaded"]
+        peer.left = dict_["left"]
+        return peer
+        
+@app.get("/announce")
+def users():
+    announcement = request.query
+    return build_listing(announcement)
