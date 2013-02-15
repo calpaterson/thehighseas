416d1c350251ece8b72aec388aab99a481beb800
Cal Paterson <cal@calpaterson.com>
2013-02-15 16:57:37 +0000

    Run script checks for redis

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-set -e
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $ROOT_DIR
+
+REDIS_UP=`echo 'PING' | redis-cli | grep PONG`
+if [[ $REDIS_UP != 0 ]]
+then
+    echo '!!!Redis is not up and running!!!'
+    exit 1
+fi
 
 echo '****Entering environment****'
 source bin/activate
