d833018866727ff73937aef870678965fa964faf
Cal Paterson <cal@calpaterson.com>
2013-02-19 10:41:31 +0000

    Add a configuration file

---
+++ config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# The High Seas Configuration File
+
+# Make sure these reflect how people access THS.
+# If they visit http://foo.com:91 these variables should match that
+export THS_HOST=localhost
+export THS_PORT=11112
