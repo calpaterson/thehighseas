8e6a55b4672b9635472124a9c464fc19a107c769
Cal Paterson <cal@calpaterson.com>
2013-02-21 17:42:36 +0000

    Change port back to 11235

--- config.sh
+++ config.sh
@@ -4,4 +4,4 @@
 # Make sure these reflect how people access THS.
 # If they visit http://foo.com:91 these variables should match that
 export THS_HOST=localhost
-export THS_PORT=11112
+export THS_PORT=11235
